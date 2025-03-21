16b7b873d0ba0337fb3e5cb035acaebb29ef3f8d
corewebsolutions <contact@corewebsolutions.com>
2025-03-21 16:41:44 -0400

    Update global.js

--- global.js
+++ global.js
@@ -44,11 +44,47 @@ document.addEventListener('DOMContentLoaded', function() {
     } else if(alertStatus === "canceled") {
 
         $('#alert-canceled').show();
-        
+
     } else if (alertStatus === "pending") {
 
         $('#alert-account-pending').show();
     }
+
+
+    // resume payment functionality 
+    $('#resume-payment').off("click");
+  
+    // Listen for clicks on any element with stripe="account" attribute
+    $('#resume-payment').on("click", function() {
+      // Store reference to the clicked element
+      const clickedButton = $(this);
+      
+      // Show loading state on the clicked element
+      const originalText = clickedButton.text();
+      clickedButton.text("Loading...").prop('disabled', true);
+      
+      // Call your Xano endpoint to get a portal session URL
+      $.ajax({
+        url: "https://xxdy-xbul-g3ez.n7d.xano.io/api:RjbKSLFK/resume_checkout",
+        type: "POST",
+        headers: {
+          "Authorization": "Bearer " + localStorage.getItem('authToken')
+        },
+        success: function(response) {
+          window.location.href = response;
+        },
+        error: function(xhr, status, error) {
+          console.error("Error:", error);
+          alert("There was an error accessing subscription management.");
+          // Reset the button that was clicked to its original state
+          clickedButton.text(originalText).prop('disabled', false);
+        }
+     });
+ 
+    });
+    
+
+
 
     // Gated content check
 
