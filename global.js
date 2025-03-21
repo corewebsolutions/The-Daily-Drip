354fe4243a2da05c7e39448ebc6e7b4aa4e22cbe
corewebsolutions <contact@corewebsolutions.com>
2025-03-21 08:06:18 -0400

    Update global.js

--- global.js
+++ global.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    //logout func
+    $('#logout-link').click(function(e) {
+        e.preventDefault();
+        localStorage.removeItem("authToken");
+        localStorage.removeItem("memberType");
+        localStorage.removeItem("status");
+        localStorage.removeItem("firstName");
+        localStorage.removeItem("lastName");
+        localStorage.removeItem("email");
+        window.location.href = "/"
+    });
+
     // check user's subscription status
     const alertStatus = localStorage.getItem("status");
        
