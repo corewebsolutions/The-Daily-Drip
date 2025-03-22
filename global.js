e3dd038917e17f0bb5f382601b52bd1db3a65386
corewebsolutions <contact@corewebsolutions.com>
2025-03-22 07:55:55 -0400

    Update global.js

--- global.js
+++ global.js
@@ -22,6 +22,8 @@ document.addEventListener('DOMContentLoaded', function() {
         $('#logout-link').show(); //show logout button
         $('#my-account-link').show(); //show my account link
         $('#alert-passwords').hide(); // remove membership update notification
+        $('#subscriber-link').hide(); // remove membership update notification
+
 
         const membership = localStorage.getItem("memberType");
         if (membership === "Subscriber") {
